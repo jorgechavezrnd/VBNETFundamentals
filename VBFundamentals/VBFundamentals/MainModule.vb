7db4f49b902c58d1974b27e976174079789a7c8c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-26 09:42:59 -0400

    Add example for Dictionary

--- VBFundamentals/VBFundamentals/MainModule.vb
+++ VBFundamentals/VBFundamentals/MainModule.vb
@@ -1,18 +1,29 @@
 ﻿Module MainModule
+
     Sub Main()
-        Dim products As New ArrayList From
-        {
-            New Product With {.ProductID = 1, .Name = "10 Speed Bike", .ProductNumber = "10-SP"},
-            New Product With {.ProductID = 2, .Name = "Bike Helmet", .ProductNumber = "BIKE-HE"},
-            New Product With {.ProductID = 3, .Name = "Inner Tube", .ProductNumber = "BIKE-IN-TU"}
-        }
+        Dim products = LoadProducts()
 
-        Console.WriteLine(DirectCast(products(0), Product).Name)
-        Console.WriteLine(DirectCast(products(1), Product).Name)
-        Console.WriteLine(DirectCast(products(2), Product).Name)
-
-        Console.WriteLine(products.Count)
+        Console.WriteLine(products(1).Name)
+        Console.WriteLine(products(2).Name)
+        Console.WriteLine(products(3).Name)
 
         Console.ReadKey()
     End Sub
+
+    Function LoadProducts() As Dictionary(Of Integer, Product)
+        Dim products As New Dictionary(Of Integer, Product)
+        Dim prod As Product
+
+        prod = New Product() With {.ProductID = 1, .Name = "10 Speed Bike", .ProductNumber = "10-SP", .ListPrice = 1431.5D}
+        products.Add(key:=prod.ProductID, value:=prod)
+
+        prod = New Product With {.ProductID = 2, .Name = "Bike Helmet", .ProductNumber = "BIKE-HE", .ListPrice = 39.99D}
+        products.Add(key:=prod.ProductID, value:=prod)
+
+        prod = New Product With {.ProductID = 3, .Name = "Inner Tube", .ProductNumber = "BIKE-IN-TU", .ListPrice = 4.99D}
+        products.Add(key:=prod.ProductID, value:=prod)
+
+        Return products
+    End Function
+
 End Module
