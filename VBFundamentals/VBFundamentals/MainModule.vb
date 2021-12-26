13aa33ff5186fa6fbc5cd110a69a90d102b1a086
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-26 09:30:35 -0400

    Add example for ArrayList

--- VBFundamentals/VBFundamentals/MainModule.vb
+++ VBFundamentals/VBFundamentals/MainModule.vb
@@ -1,13 +1,17 @@
 ﻿Module MainModule
     Sub Main()
-        Dim products As String() =
-            {"10 Speed Bike", "Bike Helmet", "Inner Tube"}
+        Dim products As New ArrayList From
+        {
+            New Product With {.ProductID = 1, .Name = "10 Speed Bike", .ProductNumber = "10-SP"},
+            New Product With {.ProductID = 2, .Name = "Bike Helmet", .ProductNumber = "BIKE-HE"},
+            New Product With {.ProductID = 3, .Name = "Inner Tube", .ProductNumber = "BIKE-IN-TU"}
+        }
 
-        Console.WriteLine(products(0))
-        Console.WriteLine(products(1))
-        Console.WriteLine(products(2))
+        Console.WriteLine(DirectCast(products(0), Product).Name)
+        Console.WriteLine(DirectCast(products(1), Product).Name)
+        Console.WriteLine(DirectCast(products(2), Product).Name)
 
-        Console.WriteLine(products.Length)
+        Console.WriteLine(products.Count)
 
         Console.ReadKey()
     End Sub
