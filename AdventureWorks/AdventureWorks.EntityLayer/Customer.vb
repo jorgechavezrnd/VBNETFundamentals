1d586fe1b070109014bd7bd6bd28f153d503f893
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-26 00:40:45 -0400

    Add example with WPF and MVVM

---
+++ AdventureWorks/AdventureWorks.EntityLayer/Customer.vb
@@ -0,0 +1,29 @@
+﻿Imports Common.Library
+
+Public Class Customer
+    Inherits CommonBase
+
+    Sub New()
+        MyBase.New()
+
+        CustomerID = 1
+    End Sub
+
+    Public Property CustomerID As Integer
+    Public Property FirstName As String
+    Public Property LastName As String
+    Public Property CompanyName As String
+
+    Protected Overrides Function GetClassData() As String
+        Dim sb As New Text.StringBuilder(1024)
+
+        sb.AppendLine("Customer ID: " + CustomerID.ToString())
+        sb.AppendLine("Company Name: " + CompanyName)
+        sb.AppendLine("First Name: " + FirstName)
+        sb.AppendLine("Last Name: " + LastName)
+        sb.AppendLine(MyBase.GetClassData())
+
+        Return sb.ToString()
+    End Function
+
+End Class
---
+++ AdventureWorks/AdventureWorks.ViewModelLayer/ProductViewModel.vb
@@ -0,0 +1,24 @@
+﻿Imports AdventureWorks.EntityLayer
+Imports Common.Library
+
+Public Class ProductViewModel
+    Inherits ViewModelBase
+
+    Sub New()
+        LoadProduct(680)
+    End Sub
+
+    Public Property Entity As Product
+
+    Function LoadProduct(ByVal productId As Integer) As Product
+        Return LoadProduct(productId, Nothing)
+    End Function
+
+    Function LoadProduct(ByVal productId As Integer, ByVal startingFilePath As String) As Product
+        ' Hard-code an entity
+        Entity = New Product() With {.ProductID = 680, .Name = "HL Road Frame - Black, 58", .ProductNumber = "FR-R92B-58", .Color = "Black", .Size = "58", .Weight = 1016.04D, .StandardCost = 1059.31D, .ListPrice = 1431.5D, .SellStartDate = #06/01/1998#, .SellEndDate = #12/31/2021#}
+
+        Return Entity
+    End Function
+
+End Class
