4e1651dbfdac449a515b6831bfda81294b41197e
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-27 00:03:50 -0400

    Add example for Region

--- VBFundamentals/VBFundamentals/MainModule.vb
+++ VBFundamentals/VBFundamentals/MainModule.vb
@@ -1,5 +1,5 @@
 ﻿Module MainModule
-
+#Region "Main Method - With End With"
     Sub Main()
         Dim prod As New Product
 
@@ -15,5 +15,23 @@
 
         Console.ReadKey()
     End Sub
+#End Region
 
+#Region "Main2 Method - With End With"
+    Sub Main2()
+        Dim prod As New Product
+
+        With prod
+            .ProductID = 1
+            .Name = "A new product"
+            .ProductNumber = "A101"
+            .StandardCost = 50
+            .ListPrice = 100
+        End With
+
+        Console.WriteLine(prod.ToString())
+
+        Console.ReadKey()
+    End Sub
+#End Region
 End Module
