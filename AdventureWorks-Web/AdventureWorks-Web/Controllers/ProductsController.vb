93eec93c381334b71b0c830caa8061352550c04c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-27 00:54:58 -0400

    Add Product List Page

---
+++ AdventureWorks-Web/AdventureWorks-Web/Controllers/ProductsController.vb
@@ -0,0 +1,16 @@
+﻿Imports AdventureWorks.ViewModelLayer
+
+Namespace Controllers
+    Public Class ProductsController
+        Inherits Controller
+
+        ' GET: Products
+        Function ProductList() As ActionResult
+            Dim vm As New ProductViewModel
+
+            vm.LoadProducts(Server.MapPath("/"))
+
+            Return View(vm)
+        End Function
+    End Class
+End Namespace
