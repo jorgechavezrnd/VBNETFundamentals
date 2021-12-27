c1284594a05e5442fc50883ab684ba4e786da8cc
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-27 00:34:12 -0400

    Add references and create the Home page

--- AdventureWorks-Web/AdventureWorks-Web/Controllers/HomeController.vb
+++ AdventureWorks-Web/AdventureWorks-Web/Controllers/HomeController.vb
@@ -4,16 +4,4 @@
     Function Index() As ActionResult
         Return View()
     End Function
-
-    Function About() As ActionResult
-        ViewData("Message") = "Your application description page."
-
-        Return View()
-    End Function
-
-    Function Contact() As ActionResult
-        ViewData("Message") = "Your contact page."
-
-        Return View()
-    End Function
 End Class
